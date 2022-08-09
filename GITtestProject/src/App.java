1ec68e493bd7d599a88cc6224d7c78627ce6775e
check <vytautas.stockus@gmail.com>
2022-08-09 10:17:58 +0300

    Added comment o greetings

--- GITtestProject/src/App.java
+++ GITtestProject/src/App.java
@@ -5,6 +5,8 @@ public class App {
 		System.out.println("Hello git");
 		
 		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		
+		//print greetings to fine falks in Midde Earth
 		for (String name:names)
 			System.out.format("Hello, %s!\n", name);
 
