425c3205536150ea3a07001436e83d43cda66e4b
check <vytautas.stockus@gmail.com>
2022-08-09 10:06:39 +0300

    Added names of LOR

--- GITtestProject/src/App.java
+++ GITtestProject/src/App.java
@@ -2,7 +2,11 @@
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello git");
+		
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		for (String name:names)
+			System.out.format("Hello, %s!\n", name);
 
 	}
 
