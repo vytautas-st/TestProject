ff20299c0baa7fcfa47be75f36539ebfad3f6061
Vytautas <vytautas.stockus@gmail.com>
2022-08-09 10:19:14 +0300

    added printout of new charackets

--- GITtestProject/src/App.java
+++ GITtestProject/src/App.java
@@ -4,9 +4,12 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		
-		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
-		for (String name:names)
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir"};
+		for (String name:names) {
 			System.out.format("Hello, %s!\n", name);
+			System.out.println("What are you doing today");
+		}
+		
 
 	}
 
